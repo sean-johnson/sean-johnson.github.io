a336e8418c4ab88eb5e9a874c6bd910a6cd0b820
Charlotte Smith <hello@seanjohnson.nz>
2016-05-29 21:10:52 +1200

    updating compatilibity issues

--- scripts/script.js
+++ scripts/script.js
@@ -9,20 +9,22 @@ document.addEventListener('mousemove', function(event) {
 
 	var rgb = [ Math.round(w), Math.round(h), 150];
 
-	var isOperaStyle = " background: -o-linear-gradient(to right top, rgba(82, 255, 184, 100), rgba(" + rgb[1] + ", " + rgb[0] + ", 155, .50));";
-	var isFirefoxStyle = " background: -moz-linear-gradient(to right top, rgba(82, 255, 184, 100), rgba(" + rgb[1] + ", " + rgb[0] + ", 155, .50));";
-	var isSafariStyle = " background: -webkit-linear-gradient(to right top, rgba(82, 255, 184, 100), rgba(" + rgb[1] + ", " + rgb[0] + ", 155, .50));";
-	var isIEStyle = " background: -ms-linear-gradient(to right top, rgba(82, 255, 184, 100), rgba(" + rgb[1] + ", " + rgb[0] + ", 155, .50));";
-	var isChromeStyle = " background: linear-gradient(to right top, rgba(82, 255, 184, 100), rgba(" + rgb[1] + ", " + rgb[0] + ", 155, .50));";
+	var isOperaGrad = " background: -o-linear-gradient(to right top, rgba(82, 255, 184, 100), rgba(" + rgb[1] + ", " + rgb[0] + ", 155, .50));";
+	var isFirefoxGrad = " background: -moz-linear-gradient(to right top, rgba(82, 255, 184, 100), rgba(" + rgb[1] + ", " + rgb[0] + ", 155, .50));";
+	var isSafariGrad = " background: linear-gradient(to right top, rgba(82, 255, 184, 100), rgba(" + rgb[1] + ", " + rgb[0] + ", 155, .50));";
+	var isIEGrad = " background: -ms-linear-gradient(to right top, rgba(82, 255, 184, 100), rgba(" + rgb[1] + ", " + rgb[0] + ", 155, .50));";
+	var isChromeGrad = " background: linear-gradient(to right top, rgba(82, 255, 184, 100), rgba(" + rgb[1] + ", " + rgb[0] + ", 155, .50));";
+	var isEdgeGrad = " background: linear-gradient(to right top, rgba(82, 255, 184, 100), rgba(" + rgb[1] + ", " + rgb[0] + ", 155, .50));";
 
 	var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 	var isFirefox = typeof InstallTrigger !== 'undefined';
 	var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
 	var isIE = /*@cc_on!@*/false || !!document.documentMode;
 	var isChrome = !!window.chrome && !!window.chrome.webstore;
+	var isEdge = !isIE && !!window.StyleMedia;
 
-	var browserArr = [isOpera, isFirefox, isSafari, isIE, isChrome];
-	var styleArr = [isOperaStyle, isFirefoxStyle, isSafariStyle, isIEStyle, isChromeStyle];
+	var browserArr = [isOpera, isFirefox, isSafari, isIE, isChrome, isEdge];
+	var styleArr = [isOperaGrad, isFirefoxGrad, isSafariGrad, isIEGrad, isChromeGrad, isEdgeGrad];
 
 
 	for (var i = 0; i <= browserArr.length; i++) {
