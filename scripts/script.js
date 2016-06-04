8752cf4f5b823f353f1468a7a5f88796e3d1d799
Charlotte Smith <hello@seanjohnson.nz>
2016-06-04 18:57:21 +1200

    portfolio page completed with links and description

--- scripts/script.js
+++ scripts/script.js
@@ -40,28 +40,29 @@ document.addEventListener('mousemove', function(event) {
 var projects = {
 
     "calculator" : {
-        "description" : "A paired project."
+        "description" : "I had fun creating a calculator for another Dev Academy assignment. Using the same styling as this blog-website, it was easy to set up this calculator's looks. However, the javascript and functionality left me stumped on a few occasions. I got there in the end and once I found my approach, it was easier to implement. Click to have a go."
     },
 
     "generator" : {
-        "description" : "A collaboration project."
+        "description" : "I did this collaboration assignment with a fellow Dev Academy student; Daniel Kermode. I contributed mostly to the CSS and HTML, Dan was the man behind the javascript. He explained to me how to extract data using APIs, get the right information—in this case it was the quote and author—and then display it within the div elements. It was awesome to learn new things. Dan was a machine. I created one of the functions that changed the coloured background when the quote-button was clicked."
     },
 
     "punchathon" : {
-        "description" : "My interpretation of the Javascript-racer. It was fun."
+        "description" : "My interpretation of the Javascript-racer assignment. We were tasked to develop a multiplayer 'racer game' that could take button inputs and progress a player closer to the finish line. In this case the finish line was the other player's face or body. I fell down many rabbit holes making this, but It was fun to do!"
     },
 
     "designSite" : {
         "description" : "My first website, before I started studying with Dev Academy. This was a site to show my design portfolio. Click to view more."
     },
 
-    "otherProjectOne" : {
-        "description" : "Project One up and running."
+    "resume" : {
+        "description" : "This assignment was an introduction to me and javascript and how it can manipulate the DOM and add information to the HTML elements. I followed the tutorials given and implemented my learning. This was the result. There are a few bugs, but all in all I mangaged to understand the learning outcomes."
     },
 
-    "otherProjectTwo" : {
-        "description" : "Let's if project two works."
+    "otherProject" : {
+        "description" : "The reaction you get when you click on this."
     }
+
 };
 
 var topInfoBox = document.getElementById("infoBoxTop");
@@ -107,13 +108,13 @@ function showInfo(obj) {
 		bottomInfoBox.className += " show";
 		bottomInfoBox.className += " right";
 		bottomInfoBox.className += " text-right";
-		bottomDescription.innerHTML = projects.otherProjectOne.description;
+		bottomDescription.innerHTML = projects.otherProject.description;
 	} 
 
 	if (x === "centerBottom") {
 		bottomInfoBox.className += " show";
 		bottomInfoBox.className += " center";
-		bottomDescription.innerHTML = projects.otherProjectTwo.description;
+		bottomDescription.innerHTML = projects.resume.description;
 	}
 
 }
