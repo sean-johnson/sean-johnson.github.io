6a387bae703f266e071a2b176efd54602d840655
sean-johnson <sean.johnson.nz@gmail.com>
2022-03-09 13:57:47 +1300

    Updates

---
+++ precache-manifest.427aca293f60806fa3b5100835f1ec82.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a4db620dec8270b2c3c0b4d781b156",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56d9ef9416ef4ba2fb18",
+    "url": "/static/js/2.3f880915.chunk.js"
+  },
+  {
+    "revision": "59dde9cf2d360ce54eb0c236051e24c5",
+    "url": "/static/js/2.3f880915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeff47f87cad1d56c69e",
+    "url": "/static/js/main.0c1437dd.chunk.js"
+  },
+  {
+    "revision": "1e0ef0fc7b9c5da7a387",
+    "url": "/static/js/runtime-main.1f922d63.js"
+  },
+  {
+    "revision": "fd3f938a23d57da8894c8aa0e85adf3f",
+    "url": "/static/media/Karla-Regular.fd3f938a.ttf"
+  },
+  {
+    "revision": "fd8aff54b599c912c460781e324d1bb0",
+    "url": "/static/media/gp-2.fd8aff54.jpg"
+  },
+  {
+    "revision": "9fbb29f73302e30deb8ca14f766c8334",
+    "url": "/static/media/gp-5.9fbb29f7.jpg"
+  },
+  {
+    "revision": "c8da9bfe84e1d86d093d9fdc1d631943",
+    "url": "/static/media/ipad-directco.c8da9bfe.jpg"
+  },
+  {
+    "revision": "7586173a89691712da1bb6e7c50d6333",
+    "url": "/static/media/mobile-platform-1@2x.7586173a.png"
+  },
+  {
+    "revision": "9c1f680b8c15e7c4e95f257d3593e99b",
+    "url": "/static/media/mobile-platform-angle-1.9c1f680b.png"
+  },
+  {
+    "revision": "67cfbfc871f0712e4f12bb2fab9ed300",
+    "url": "/static/media/quantum-4.67cfbfc8.jpg"
+  },
+  {
+    "revision": "9e5684270fa30ecf267768f9f1a1caa6",
+    "url": "/static/media/studio-pleasure.9e568427.png"
+  },
+  {
+    "revision": "a6d70fa486e5f683d96b73421531f992",
+    "url": "/static/media/thweeknd-2.a6d70fa4.jpg"
+  },
+  {
+    "revision": "0d2f6c3420016088e268556b33432e95",
+    "url": "/static/media/type-2.0d2f6c34.jpg"
+  },
+  {
+    "revision": "a5d5ae3dc30aea2e18284fc6f93c5fde",
+    "url": "/static/media/various-5.a5d5ae3d.jpg"
+  },
+  {
+    "revision": "106124f489284f12754bf299f4504ea3",
+    "url": "/static/media/various-7.106124f4.jpg"
+  },
+  {
+    "revision": "8837696fe432852728a5ae3580bc08e2",
+    "url": "/static/media/web-platform-1@2x.8837696f.png"
+  }
+]);
