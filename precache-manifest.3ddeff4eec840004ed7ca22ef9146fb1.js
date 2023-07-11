b5d42457afdd16da2756f1c8e702e143dd11c706
sean-johnson <sean.johnson.nz@gmail.com>
2023-07-11 17:15:16 +1200

    Updates

---
+++ precache-manifest.3ddeff4eec840004ed7ca22ef9146fb1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfb005d0c97afbd595a60c31ab5c3c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbbb2f5111d5069ca757",
+    "url": "/static/js/2.c5ee0290.chunk.js"
+  },
+  {
+    "revision": "59dde9cf2d360ce54eb0c236051e24c5",
+    "url": "/static/js/2.c5ee0290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67bf54497dd2ba864da3",
+    "url": "/static/js/main.64449d9d.chunk.js"
+  },
+  {
+    "revision": "1e0ef0fc7b9c5da7a387",
+    "url": "/static/js/runtime-main.1f922d63.js"
+  },
+  {
+    "revision": "fd3f938a23d57da8894c8aa0e85adf3f",
+    "url": "/static/media/Karla-Regular.fd3f938a.ttf"
+  },
+  {
+    "revision": "c956be8c1821df06d65d5438340faf6b",
+    "url": "/static/media/image-1@2x.c956be8c.png"
+  },
+  {
+    "revision": "b41c4fc6c5a49fbad87eb544054bc7a6",
+    "url": "/static/media/image-2@2x.b41c4fc6.png"
+  },
+  {
+    "revision": "275b74a60ef22f680cd6320e08887e9b",
+    "url": "/static/media/image-3@2x.275b74a6.png"
+  },
+  {
+    "revision": "21fb7a5834312998f1d01025b72098e7",
+    "url": "/static/media/image-4@2x.21fb7a58.png"
+  },
+  {
+    "revision": "258bbd58c1f8ade681b97f9b72a8589a",
+    "url": "/static/media/image-5@2x.258bbd58.png"
+  },
+  {
+    "revision": "a0a4781b7bc084747592a846f937b5c7",
+    "url": "/static/media/image-6@2x.a0a4781b.png"
+  },
+  {
+    "revision": "14db2895c4b3c612d0ae32d22ce804ef",
+    "url": "/static/media/image-7@2x.14db2895.png"
+  },
+  {
+    "revision": "80c653ba880b92baa375da9b01bf5ceb",
+    "url": "/static/media/image-8@2x.80c653ba.png"
+  }
+]);
