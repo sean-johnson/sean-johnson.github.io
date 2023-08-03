b0716c3ddd3fabda1cbfedde0f9b371995a3fdda
sean-johnson <sean.johnson.nz@gmail.com>
2023-08-03 15:42:45 +1200

    edits to CV

--- src/components/ViewDocument/ViewDocument.js
+++ src/components/ViewDocument/ViewDocument.js
@@ -67,6 +67,17 @@ const MyResume = () => (
         <View style={documentStyles.rightTopSection}>
           <Text style={documentStyles.tagline}></Text>
         </View>
+        <Text style={documentStyles.descriptionHeading}>Essential Traits</Text>
+
+        {/* <Text style={documentStyles.descriptionYear}>Soft skills, work life day to day</Text> */}
+        <Text style={documentStyles.description}>
+          I take pride in good communication and active listening, which enable me to build strong relationships and trust.
+          Collaborating on work energises me, as I enjoy sharing ideas, solving problems creatively and delivering value together. 
+        </Text>
+        <Text style={documentStyles.description}>
+          Working in Tech exposes me to constant change, so adopting a growth mindset is essential to me. It also fosters a healthy a work culture where feedback and challenges are seen as opportunities for improvement. 
+          If there are high-pressure situations, I tend to approach them with a calm, solution-focused attitude.
+        </Text>
         <Text style={documentStyles.descriptionHeading}>Experience</Text>
 
         <Text style={documentStyles.descriptionRole}> <Text style={documentStyles.descriptionBold}>Taskhunter</Text> — Co-founder, Lead Full Stack Engineer & Designer</Text>
@@ -83,7 +94,7 @@ const MyResume = () => (
         <Text style={documentStyles.descriptionRole}> <Text style={documentStyles.descriptionBold}>Directco</Text> — Co-founder, Lead Full Stack Engineer & Designer</Text>
         <Text style={documentStyles.descriptionYear}>2016 – Present</Text>
         <Text style={documentStyles.description}>
-          Started as lead designer and full stack developer, eventually becoming lead full stack engineer.
+          I started as lead designer and full stack developer, eventually becoming lead full stack engineer.
           Responsible for building of web and mobile applications, AWS infrastructure services, and design. {"\n"}
           Successfully built services from concept to product, serving major FMCG clients like Fonterra, Red Bull, Unilever, Asahi, Bidfood, and others.
         </Text>
@@ -91,7 +102,7 @@ const MyResume = () => (
         <Text style={documentStyles.description}>
           I played a core role in building a secure, modern tech stack that guaranteed high security standards for our global enterprise clients. Our tech stack included
           React (JavaScript), NodeJS, MySQL, Docker, React-native (for mobile development) and other technologies. AWS services were used for infrastructure, including EC2, S3, Loadbalancing, Quicksight and others.
-          We also established API endpoints and SFTP integrations for enterprise integrations, facilitating seamless data exchange with external systems.
+          We also established API endpoints and SFTP integrations for enterprises, facilitating seamless data exchange with external systems.
         </Text>
 
         <Text style={documentStyles.descriptionRole}>
@@ -100,7 +111,8 @@ const MyResume = () => (
         </Text>
         <Text style={documentStyles.descriptionYear}>2012 – 2016</Text>
         <Text style={documentStyles.description}>
-          Designer with a specialisation in branding. Built brands for clients.
+          My role was a Designer with a specialisation in branding. I built brands for clients which required me to 
+          faciliate various clients in many industries and be able to digest their business values and translate them innovatively to their customers.
         </Text>
 
         <Text style={documentStyles.descriptionRole}>
@@ -120,14 +132,16 @@ const MyResume = () => (
         <Text style={documentStyles.descriptionRole}><Text style={documentStyles.descriptionBold}>University of Waikato</Text> — Masters in Design</Text>
         <Text style={documentStyles.descriptionYear}>2008 – 2012</Text>
         <Text style={documentStyles.description}>
-          Learned how to creatively and methodically solve problems. {"\n"}
+          I learned how to apply design thinking to solve problems creatively and communicate ideas through any medium.
           Graduated with Masters specialising in Branding and Typography.
         </Text>
 
         <Text style={documentStyles.descriptionRole}><Text style={documentStyles.descriptionBold}>Devacademy</Text> — Software Developer Graduate</Text>
         <Text style={documentStyles.descriptionYear}>2016</Text>
         <Text style={documentStyles.description}>
-          Full stack web development bootcamp. Intense real-world projects built with agile workflows and teams.
+          Full stack web development bootcamp. Intense real-world projects built with agile workflows and teams. 
+          Apart from learning to build {"\n"}software, a significant portion was
+          devoted to soft skills, learning how to learn, interacting as a team and working with agile methodologies.
         </Text>
 
         <Text style={documentStyles.descriptionRole}><Text style={documentStyles.descriptionBold}>Scrum.org</Text> — Professional Scrum Master I</Text>
